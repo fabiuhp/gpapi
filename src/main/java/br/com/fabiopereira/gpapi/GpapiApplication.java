4a8f4b8d4bf7ecba53483f8a74564e3a92b35941
Fabio Pereira <fabiuhp@msn.com>
2024-10-20 11:05:27 -0300

    init

---
+++ src/main/java/br/com/fabiopereira/gpapi/GpapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiopereira.gpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpapiApplication.class, args);
+	}
+
+}
